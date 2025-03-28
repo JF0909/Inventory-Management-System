ddbb1ed7e88b9182c872a0073e415a9ded5aebb1
Jingwen Fu <n11886609@qut.edu.au>
2025-03-28 16:47:59 +1000

    modify the axios in frontend

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 // create axios instance
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001'
-  baseURL: 'http://3.27.240.159:5001',
+  baseURL: 'http://3.26.10.113:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
