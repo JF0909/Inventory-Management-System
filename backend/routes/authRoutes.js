e7d19a4beb256714034ec9329adeba8830ffd897
Jingwen Fu <n11886609@qut.edu.au>
2025-04-04 12:30:47 +1000

    delete unnecessary models declare

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../models/User')
+//const User = require('../models/User')
 const { signup, login } = require('../controllers/authController');
 
 const router = express.Router();
